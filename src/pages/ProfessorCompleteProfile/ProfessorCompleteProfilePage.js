48526e1c9578a4c1a032b97d718c194f2c964c6e
kasun7153 <kasun7153@gmail.com>
2021-08-28 15:46:33 +0530

    gg

--- src/pages/ProfessorCompleteProfile/ProfessorCompleteProfilePage.js
+++ src/pages/ProfessorCompleteProfile/ProfessorCompleteProfilePage.js
@@ -2,17 +2,48 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
-import { Checkbox, Grid, TextField } from '@material-ui/core';
+import { Checkbox, Grid, MenuItem, Select, TextField } from '@material-ui/core';
 import { useHistory, useLocation } from 'react-router-dom';
 import Header from '../../common/Header';
 import Footer from '../Home/components/Footer';
+
+const departments = [
+  {
+    department: '',
+    topics: [],
+  },
+  {
+    department: 'Computer Science',
+    topics: ['HTML', 'MongoDB', 'Django', 'NoSQL', 'ASP.NET'],
+  },
+  {
+    department: 'Management',
+    topics: [
+      'Leadership',
+      'Motivation & Job Satisfaction',
+      'Emotional Intelligence',
+      'Communication',
+      'Groups & Teams',
+      'Recruiting',
+      'Compensation',
+      'Training',
+      'Cross-cultural management',
+      'Mission, vision, & goal-setting',
+      'designing & formulating strategy',
+      'Strategy implementation',
+      'Corporate culture',
+      'Organizational structure',
+      'Innovation',
+    ],
+  },
+];
 
 const useStyles = makeStyles(() => ({
   btn: {
     background: '#00b074',
     borderRadius: 5,
     padding: '13px 0px',
-    width: '100%',
+    width: '200px',
     textAlign: 'center',
     cursor: 'pointer',
     color: 'white',
@@ -26,9 +57,22 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-function HomePage({ match }) {
+function HomePage() {
   const dispatch = useDispatch();
   const history = useHistory();
+
+  const [schoolName, setSchoolName] = React.useState('');
+  const [campusLocation, setCampusLocation] = React.useState('');
+  const [college, setCollege] = React.useState('');
+  const [discipline, setDiscipline] = React.useState('');
+  const [myDepartment, setMyDepartment] = React.useState('');
+
+  const [selectedDepartment1, setSelectedDepartment1] = React.useState('');
+  const [selectedTopics1, setSelectedTopics1] = React.useState([]);
+
+  const [selectedDepartment2, setSelectedDepartment2] = React.useState('');
+  const [selectedTopics2, setSelectedTopics2] = React.useState([]);
+
   const [wantGuestLecturer, setWantGuestLecturer] = React.useState({
     checked: false,
     inPerson: true,
@@ -42,11 +86,56 @@ function HomePage({ match }) {
 
   const classes = useStyles();
 
+  const submit = () => {
+    const req = {
+      schoolName,
+      college,
+      department: myDepartment,
+      campusLocation,
+      discipline,
+      role: [],
+      recruitingDepartment: [],
+    };
+
+    if (wantGuestLecturer.checked) {
+      const reqWantGuestLecturer = {
+        type: 'Guest Lecturer',
+        inPerson: wantGuestLecturer.inPerson,
+        zoom: wantGuestLecturer.zoom,
+      };
+      req.role.push(reqWantGuestLecturer);
+    }
+    if (wantAdjunctProfessor.checked) {
+      const reqWantAdjunctProfessor = {
+        type: 'Adjunct Professor',
+        inPerson: wantAdjunctProfessor.inPerson,
+        zoom: wantAdjunctProfessor.zoom,
+      };
+      req.role.push(reqWantAdjunctProfessor);
+    }
+    if (selectedDepartment1) {
+      const reqSelectedDepartment1 = {
+        department: selectedDepartment1,
+        topics: selectedTopics1,
+      };
+      req.recruitingDepartment.push(reqSelectedDepartment1);
+    }
+
+    if (selectedDepartment2) {
+      const reqSelectedDepartment2 = {
+        department: selectedDepartment2,
+        topics: selectedTopics2,
+      };
+      req.recruitingDepartment.push(reqSelectedDepartment2);
+    }
+    console.log(req);
+  };
+
   return (
     <div>
       <Header />
-      <div>
-        <div style={{ margin: '30px 20%' }}>
+      <div style={{ margin: '30px 20%' }}>
+        <div>
           <div
             style={{
               fontSize: 40,
@@ -57,53 +146,9 @@ function HomePage({ match }) {
           >
             Complete Your Profile
           </div>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <span className={classes.formTitle}>Personal Information</span>
-            </Grid>
-            <Grid item xs={4}>
-              <TextField
-                inputProps={{
-                  style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
-                }}
-                style={{ background: 'white' }}
-                fullWidth
-                size="small"
-                type="text"
-                variant="outlined"
-                placeholder="User Name"
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <TextField
-                inputProps={{
-                  style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
-                }}
-                style={{ background: 'white' }}
-                fullWidth
-                size="small"
-                type="text"
-                variant="outlined"
-                placeholder="User Name"
-              />
-            </Grid>
-            <Grid item xs={4}>
-              <TextField
-                inputProps={{
-                  style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
-                }}
-                style={{ background: 'white' }}
-                fullWidth
-                size="small"
-                type="text"
-                variant="outlined"
-                placeholder="User Name"
-              />
-            </Grid>
-          </Grid>
 
           {/* University or School Information */}
-          <Grid container spacing={2} style={{ marginTop: 20 }}>
+          <Grid container spacing={2} style={{ marginTop: 40 }}>
             <Grid item xs={12}>
               <span className={classes.formTitle}>
                 University or School Information
@@ -111,6 +156,8 @@ function HomePage({ match }) {
             </Grid>
             <Grid item xs={4}>
               <TextField
+                value={schoolName}
+                onChange={(e) => setSchoolName(e.target.value)}
                 inputProps={{
                   style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
                 }}
@@ -124,6 +171,8 @@ function HomePage({ match }) {
             </Grid>
             <Grid item xs={4}>
               <TextField
+                value={campusLocation}
+                onChange={(e) => setCampusLocation(e.target.value)}
                 inputProps={{
                   style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
                 }}
@@ -137,6 +186,8 @@ function HomePage({ match }) {
             </Grid>
             <Grid item xs={4}>
               <TextField
+                value={college}
+                onChange={(e) => setCollege(e.target.value)}
                 inputProps={{
                   style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
                 }}
@@ -150,6 +201,8 @@ function HomePage({ match }) {
             </Grid>
             <Grid item xs={8}>
               <TextField
+                value={discipline}
+                onChange={(e) => setDiscipline(e.target.value)}
                 inputProps={{
                   style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
                 }}
@@ -163,6 +216,8 @@ function HomePage({ match }) {
             </Grid>
             <Grid item xs={4}>
               <TextField
+                value={myDepartment}
+                onChange={(e) => setMyDepartment(e.target.value)}
                 inputProps={{
                   style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
                 }}
@@ -177,7 +232,7 @@ function HomePage({ match }) {
           </Grid>
 
           {/* What role do you need to fill? */}
-          <Grid container spacing={2} style={{ marginTop: 20 }}>
+          <Grid container spacing={2} style={{ marginTop: 40 }}>
             <Grid item xs={12}>
               <span className={classes.formTitle}>
                 What role do you need to fill?
@@ -277,26 +332,142 @@ function HomePage({ match }) {
           </Grid>
 
           {/* For which Functional department are you recruiting */}
-          <Grid container spacing={2} style={{ marginTop: 20 }}>
+          <Grid container spacing={2} style={{ marginTop: 40 }}>
             <Grid item xs={12}>
               <span className={classes.formTitle}>
                 For which Functional department are you recruiting?
               </span>
             </Grid>
-            <Grid item xs={4}>
-              <TextField
-                inputProps={{
-                  style: { WebkitBoxShadow: '0 0 0 1000px white inset' },
+            <Grid item xs={5}>
+              Department
+              <Select
+                value={selectedDepartment1}
+                onChange={(e) => {
+                  setSelectedDepartment1(e.target.value);
+                  setSelectedTopics1([]);
                 }}
-                style={{ background: 'white' }}
-                fullWidth
-                size="small"
-                type="text"
                 variant="outlined"
-                placeholder="User Name"
-              />
+                inputProps={{
+                  name: 'age',
+                  id: 'age-native-simple',
+                }}
+                style={{ width: 200, marginLeft: 30 }}
+              >
+                {departments.map((department, index) => (
+                  <MenuItem
+                    // disabled={selectedTopics1.indexOf(topic) <= -1}
+                    value={department.department}
+                    key={index}
+                    style={{ height: 40 }}
+                  >
+                    <span>{department.department}</span>
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item xs={7} style={{ textAlign: 'right' }}>
+              Topics
+              <Select
+                variant="outlined"
+                multiple
+                disabled={selectedDepartment1 === ''}
+                value={selectedTopics1}
+                renderValue={(selected) => selected.join(', ')}
+                onChange={(e) => {
+                  setSelectedTopics1(e.target.value);
+                }}
+                inputProps={{
+                  name: 'age',
+                  id: 'age-native-simple',
+                }}
+                style={{ width: 400, marginLeft: 30 }}
+              >
+                {departments
+                  .filter((el) => el.department === selectedDepartment1)[0]
+                  .topics.map((topic, index) => (
+                    <MenuItem
+                      // disabled={selectedTopics1.indexOf(topic) <= -1}
+                      value={topic}
+                      key={index}
+                    >
+                      <Checkbox checked={selectedTopics1.indexOf(topic) > -1} />
+                      <span>{topic}</span>
+                    </MenuItem>
+                  ))}
+              </Select>
+            </Grid>
+
+            {/* fdsfdsfdsfdsfsdff */}
+            <Grid item xs={5}>
+              Department
+              <Select
+                value={selectedDepartment2}
+                onChange={(e) => {
+                  setSelectedDepartment2(e.target.value);
+                  setSelectedTopics2([]);
+                }}
+                variant="outlined"
+                inputProps={{
+                  name: 'age',
+                  id: 'age-native-simple',
+                }}
+                style={{ width: 200, marginLeft: 30 }}
+              >
+                {departments.map((department, index) => (
+                  <MenuItem
+                    // disabled={selectedTopics1.indexOf(topic) <= -1}
+                    value={department.department}
+                    key={index}
+                    style={{ height: 40 }}
+                  >
+                    <span>{department.department}</span>
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
+            <Grid item xs={7} style={{ textAlign: 'right' }}>
+              Topics
+              <Select
+                variant="outlined"
+                multiple
+                disabled={selectedDepartment2 === ''}
+                value={selectedTopics2}
+                renderValue={(selected) => selected.join(', ')}
+                onChange={(e) => {
+                  setSelectedTopics2(e.target.value);
+                }}
+                inputProps={{
+                  name: 'age',
+                  id: 'age-native-simple',
+                }}
+                style={{ width: 400, marginLeft: 30 }}
+              >
+                {departments
+                  .filter((el) => el.department === selectedDepartment2)[0]
+                  .topics.map((topic, index) => (
+                    <MenuItem
+                      // disabled={selectedTopics1.indexOf(topic) <= -1}
+                      value={topic}
+                      key={index}
+                    >
+                      <Checkbox checked={selectedTopics2.indexOf(topic) > -1} />
+                      <span>{topic}</span>
+                    </MenuItem>
+                  ))}
+              </Select>
             </Grid>
           </Grid>
+        </div>
+        <div
+          style={{
+            marginTop: 40,
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <div className={classes.btn} onClick={submit}>
+            Save
+          </div>
         </div>
       </div>
       <Footer />
